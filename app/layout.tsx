ea42d2ccc3961a7372e54012c468d0fe18206d0c
Debadeep Sen (MSIK) <debadeep.sen@gmail.com>
2025-08-10 18:49:49 +0530

    added simple analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,10 @@ export default function RootLayout({
           href='https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap'
           rel='stylesheet'
         ></link>
+        <script
+          async
+          src='https://scripts.simpleanalyticscdn.com/latest.js'
+        ></script>
       </head>
       <body>{children}</body>
     </html>
